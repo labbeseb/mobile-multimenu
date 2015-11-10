a7b54ec4bdfe8687906f5d7159f97184903e6fe2
slabbe <sebastien@audivox.fr>
2015-11-10 11:38:35 +0100

    base

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+
+        uglify: {
+            dist: {
+                files: {
+                    'assets/js/mobilemultimenu.min.js': 'assets/js/mobilemultimenu.js'
+                }
+            }
+        },
+
+        cssmin: {
+            mini: {
+                options: {
+                    keepSpecialComments: '0'
+                },
+                files: {
+                    'assets/css/mobilemultimenu.min.css': 'assets/css/mobilemultimenu.css'
+                }
+            }
+        },
+
+        less: {
+            development: {
+                options: {
+                    paths: ["assets/css"]
+                },
+                files: {
+                    "assets/css/mobilemultimenu.css": "assets/less/mobilemultimenu.less"
+                }
+            }
+        },
+
+
+        watch: {
+            convLess: {
+                files: ['assets/less/*.less'],
+                tasks: ['less'],
+                options: {}
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+};
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Mx3 - Mobile Multi Menu</title>
+
+    <link rel="stylesheet" href="/assets/css/mobilemultimenu.css" media="all">
+</head>
+
+<body>
+
+    <button type="button" class="3xm">Show menu</button>
+
+    <script type="text/javascript" src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="/assets/js/mobilemultimenu.js"></script>
+</body>
+</html>
